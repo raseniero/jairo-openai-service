e0791022bf8f405f4d53416c7ea0006588c6781a
Ramon Aseniero <raseniero@gmail.com>
2023-06-08 13:43:43 -0700

    added chatgpt model

--- chatgpt/admin.py
+++ chatgpt/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import ChatGPT  # Import the ChatGPT model from chatgpt/models.py
 
 # Register your models here.
+admin.site.register(ChatGPT)  # Register the ChatGPT model with the admin site
--- chatgpt/models.py
+++ chatgpt/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+class ChatGPT(models.Model):
+    system_input = models.CharField(max_length=100)
+    user_input = models.CharField(max_length=100)
+    bot_response = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "chatgpt"
+        verbose_name = "ChatGPT"
+        verbose_name_plural = "ChatGPTs"
+
+    def __str__(self):
+        return self.user_input
