a5c706973684d37d0406e50df8bee116de751928
ramon <raseniero@gmail.com>
2023-06-10 19:34:38 -0700

    added static assets

--- jairo_openai_service/urls.py
+++ jairo_openai_service/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include  # Import the include function from django.urls
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("chatgpt.urls")),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
