b874eab59fd4a2ccf3d67411dda090ca04b809ef
ntuban103091 <onicsbantu@gmail.com>
2023-07-14 11:29:48 +0800

    modify srt_fcpxml_converter.py - asset & video

--- srt_converter_app/srt_fcpxml_converter.py
+++ srt_converter_app/srt_fcpxml_converter.py
@@ -1,9 +1,10 @@
 import os
 import base64
 import re
-from django.core.files.uploadedfile import SimpleUploadedFile
+import uuid
+#from django.core.files.uploadedfile import SimpleUploadedFile
 
-def convert_to_fcpxml(timecode_description, images_base_path):
+def convert_to_fcpxml(timecode_description):
     current_dir = os.path.dirname(os.path.abspath(__file__))
     template_path = os.path.join(current_dir, 'Template.xml')    
     
@@ -14,8 +15,8 @@ def convert_to_fcpxml(timecode_description, images_base_path):
     # Replace placeholders in the template with the provided timecode and description
     title_nos = re.findall(r'^(\d+)$', timecode_description, re.MULTILINE)
     timecodes = re.findall(r'(\d\d:\d\d:\d\d,\d\d\d) --> (\d\d:\d\d:\d\d,\d\d\d)', timecode_description)
-    descriptions = re.findall(r'<b>(.*?)</b>', timecode_description)
-    image_names = re.findall(r'img src="([^"]+)"', timecode_description)
+    images = re.findall(r'<b>(.*?)</b>', timecode_description)
+   
 
 
     event_name = 'CC_XML'  # Replace with the desired event name
@@ -24,51 +25,57 @@ def convert_to_fcpxml(timecode_description, images_base_path):
     fcpxml_content = template_content.replace('{EVENT_NAME}', event_name)
     fcpxml_content = fcpxml_content.replace('{PROJECT_NAME}', project_name)
 
-    title_template = '''
-        <title name="{TITLE_NO}" offset="{OFFSET}" ref="r2" duration="{DURATION}" start="{START}">
-            <param name="Position" key="9999/10199/10201/1/100/101" value="0 -418.279"/>
-            <param name="Alignment" key="9999/10199/10201/2/354/1002961760/401" value="1 (Center)"/>
-            <param name="Alignment" key="9999/10199/10201/2/373" value="0 (Left) 2 (Bottom)"/>
-            <param name="Out Sequencing" key="9999/10199/10201/4/10233/201/202" value="0 (To)"/>
-            <param name="Wrap Mode" key="9999/10199/10201/5/10203/21/25/5" value="1 (Repeat)"/>
-            <param name="Color" key="9999/10199/10201/5/10203/30/32" value="0 0 0"/>
-            <param name="Wrap Mode" key="9999/10199/10201/5/10203/30/34/5" value="1 (Repeat)"/>
-            <param name="Width" key="9999/10199/10201/5/10203/30/36" value="3"/>
-            <text>
-                <text-style ref="ts1">{DESCRIPTION}</text-style>
-            </text>
-            <image ref="image{TITLE_NO}" offset="0s" duration="{DURATION}" start="{START}" src="" /> 
-            <text-style-def id="ts1">
-                <text-style font="Arial" fontSize="50" fontFace="Regular" fontColor="0.999996 1 1 1" shadowColor="0 0 0 0.75" shadowOffset="5 315" alignment="center"/>
-            </text-style-def>
-        </title>
+
+    asset_template = '''
+            <asset start="0/1s" name="{IMAGE}" id="{TITLE_NO}" duration="{DURATION}" hasVideo="1" format="r3">
+                <media-rep kind="original-media" src="{IMAGE}"/>
+            </asset>
     '''
 
+    spine_template = '''
+            <video offset="{OFFSET}" start="0/1s" name="{IMAGE}" ref="{TITLE_NO}" duration="{DURATION}" enabled="1">
+                    <adjust-transform anchor="0 0" position="0 0" scale="1 1"/>
+            </video> 
+    '''
     # Initialize the fcpxml_content with template_content
     fcpxml_content = template_content
 
     spine = ''
-    for i, (timecode, description, title_no, image_name) in enumerate(zip(timecodes, descriptions, title_nos,image_names), 1):
+    asset = ''
+    for i, (timecode, image, title_no) in enumerate(zip(timecodes, images, title_nos), 1):
         start, end = timecode
 
          # Convert timecode duration to offset format
         #start = convert_timecode_to_offset(start)  # Convert timecode to offset format
         offset = convert_timecode_to_offset(end)  # Convert timecode to offset format
         duration = convert_timecode_to_offset_duration(start, end)  # Convert timecode duration to offset format
-        image_path = os.path.join(images_base_path, image_name)  
+        asset_id = str(uuid.uuid4())  # Generate a unique ID for the asset
 
-        title_content = title_template.format(
+        spine_content = spine_template.format(
             TITLE_NO=title_no,
             OFFSET=offset,
             DURATION=duration,
             START=start,
-            DESCRIPTION=description,
-            IMAGE_PATH=image_path
+            IMAGE=image,
+           
         )
 
-        spine += title_content
+        asset_content = asset_template.format(
+            TITLE_NO=title_no,
+            OFFSET=offset,
+            DURATION=duration,
+            ASSET_ID=asset_id,
+            START=start,
+            IMAGE=image,
+           
+        )
 
-    fcpxml_content = fcpxml_content.replace('{result}',spine)
+        spine += spine_content
+        asset += asset_content
+
+    fcpxml_content = fcpxml_content.replace('{spine_result}', spine)
+    fcpxml_content = fcpxml_content.replace('{asset_result}', asset )
+
 
     # Convert the modified FCPXML content to a base64-encoded string
     fcpxml_bytes = fcpxml_content.encode('utf-8')
