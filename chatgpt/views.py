56efdaea905ca2e327141a9171a677937c2aa2c3
ntuban@contractors.stevensonsystems.com <ntuban@contractors.stevensonsystems.com>
2023-07-25 15:09:45 +0800

    rebuild the timecode converter

--- chatgpt/views.py
+++ chatgpt/views.py
@@ -11,10 +11,6 @@ import openai
 from bs4 import BeautifulSoup
 from rest_framework.views import APIView
 
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
-
 
 from django.http import JsonResponse
 
