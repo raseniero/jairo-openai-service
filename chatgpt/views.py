dfbe938230044cbafcc6e87a0149848a00b37b12
ntuban103091 <onicsbantu@gmail.com>
2023-10-05 11:13:15 +0800

    test push

--- chatgpt/views.py
+++ chatgpt/views.py
@@ -2,7 +2,7 @@
 Module to define views for chatgpt
 """
 from datetime import datetime
-import pkg_resources
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
